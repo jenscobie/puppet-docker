de4a4cd6d7f7da11c0e153e56e4fb7fdf14def75
jenscobie <jenscobie@gmail.com>
2015-09-26 15:35:14 -0700

    Test fix

--- spec/classes/docker_spec.rb
+++ spec/classes/docker_spec.rb
@@ -10,7 +10,7 @@ describe 'docker' do
       should contain_package('DockerToolbox-1.8.2b').with({
         :ensure   => 'present',
         :source   => 'https://github.com/docker/toolbox/releases/download/v1.8.2b/DockerToolbox-1.8.2b.pkg',
-        :provider => 'apple',
+        :provider => 'pkgdmg',
         :install_options => ['--appdir=/Applications', '--binarydir=/test/boxen/bin'],
       })
     end
